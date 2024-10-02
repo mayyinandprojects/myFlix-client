8efdf710acf94f8505e0549ea48958a7e8676f0f
may_ <lammayyin@gmail.com>
2024-10-03 00:14:25 +0200

    add loading skeleton
    add loading skeleton to main view and movie view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,6 +11,10 @@ import { ProfileView } from "../profile-view/profile-view";
 import { Link } from "react-router-dom";
 import Alert from "react-bootstrap/Alert";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { SkeletonTheme } from 'react-loading-skeleton';
+
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -22,7 +26,8 @@ export const MainView = () => {
   const [user, setUser] = useState(storedUser);
   const [token, setToken] = useState(storedToken);
   const [users, setUsers] = useState([]);
-  const [filter, setFilter] = useState(""); // Filter state
+  const [filter, setFilter] = useState("");
+  const [loading, setLoading] = useState(true);
 
   // Function to handle user logout
   const onLoggedOut = () => {
@@ -90,10 +95,12 @@ export const MainView = () => {
           rating: movie.rating,
         }));
         setMovies(moviesFromApi);
+        // setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching movies:", error);
         setError(error.message);
+        // setLoading(false);
       });
 
     fetch("https://movie-api-4o5a.onrender.com/users", {
@@ -116,10 +123,12 @@ export const MainView = () => {
           favoriteMovies: user.favorite_movies || [],
         }));
         setUsers(usersFromApi);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching users:", error);
         setError(error.message);
+        setLoading(false);
       });
   }, [token]);
 
@@ -138,8 +147,9 @@ export const MainView = () => {
   );
 
   return (
+    <SkeletonTheme baseColor="#e0e0e0" highlightColor="#f5f5f5">
     <BrowserRouter>
-      <NavigationBar user={user} onLoggedOut={onLoggedOut} />
+      <NavigationBar user={user} onLoggedOut={() => localStorage.clear()} />
       <Row className="justify-content-md-center mt-5">
         <Routes>
           <Route
@@ -186,11 +196,13 @@ export const MainView = () => {
             element={
               !user ? (
                 <Navigate to="/login" replace />
-              ) : movies.length === 0 ? (
-                <Col>The list is empty!</Col>
               ) : (
                 <Col md={8}>
-                  <MovieView movies={movies} />
+                  {loading ? (
+                    <Skeleton height={600} width={400} />
+                  ) : (
+                    <MovieView movies={movies} />
+                  )}
                 </Col>
               )
             }
@@ -206,7 +218,6 @@ export const MainView = () => {
                     users={users}
                     favoriteMovies={favoriteMovies}
                     handleFavoriteToggle={handleFavoriteToggle}
-                    setFavoriteMovies={setFavoriteMovies}
                   />
                 </Col>
               )
@@ -237,6 +248,20 @@ export const MainView = () => {
                     >
                       No movies match your search!
                     </Alert>
+                    <Row>
+                      <Col md={3}>
+                        <Skeleton height={550} />
+                      </Col>
+                      <Col md={3}>
+                        <Skeleton height={550} />
+                      </Col>
+                      <Col md={3}>
+                        <Skeleton height={550} />
+                      </Col>
+                      <Col md={3}>
+                        <Skeleton height={550} />
+                      </Col>
+                    </Row>
                   </>
                 ) : (
                   <>
@@ -253,17 +278,28 @@ export const MainView = () => {
                         />
                       </Col>
                     </Row>
-
-                    {filteredMovies.map((movie) => (
-                      <Col className="mb-5" key={movie.id} md={3}>
-                        <MovieCard
-                          movie={movie}
-                          isFavorite={favoriteMovies.includes(String(movie.id))}
-                          onFavoriteToggle={handleFavoriteToggle}
-                          username={user.username}
-                        />
-                      </Col>
-                    ))}
+                    {loading ? (
+                      <Row>
+                        {[...Array(4)].map((_, idx) => (
+                          <Col key={idx} className="mb-5" md={3}>
+                            <Skeleton height={800} width={600} />
+                          </Col>
+                        ))}
+                      </Row>
+                    ) : (
+                      filteredMovies.map((movie) => (
+                        <Col className="mb-5" key={movie.id} md={3}>
+                          <MovieCard
+                            movie={movie}
+                            isFavorite={favoriteMovies.includes(
+                              String(movie.id)
+                            )}
+                            onFavoriteToggle={handleFavoriteToggle}
+                            username={user.username}
+                          />
+                        </Col>
+                      ))
+                    )}
                   </>
                 )
               ) : (
@@ -274,6 +310,7 @@ export const MainView = () => {
         </Routes>
       </Row>
     </BrowserRouter>
+    </SkeletonTheme>
   );
 };
 
