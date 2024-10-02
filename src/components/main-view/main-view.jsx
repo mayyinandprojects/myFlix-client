463959a7360c2a95eb385e104843ab9684b1811e
may_ <lammayyin@gmail.com>
2024-10-02 20:21:59 +0200

    debug parcelRequire

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 
