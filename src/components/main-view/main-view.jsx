222e2a059d2569307110d0bca8fda146787faf05
may_ <lammayyin@gmail.com>
2024-10-03 00:55:17 +0200

    update index.html

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -20,7 +20,6 @@ export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
   const [error, setError] = useState(null);
   const [user, setUser] = useState(storedUser);
